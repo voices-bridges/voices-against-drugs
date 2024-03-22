11f7ebc38cc6af1a0a92f5050a34737ba1334e5a
ww <ww@voicesandbridges.onmicrosoft.com>
2024-03-21 21:24:51 -0700

    Added VCH support timelines.

--- src/components/react/richmond_scs.js
+++ src/components/react/richmond_scs.js
@@ -28,6 +28,20 @@ We urge you to reconsider this decision for the sake of all Richmond residents' 
   },
   {
     title: "2024-02-05",
+    cardTitle: "VCH's support",
+    cardSubtitle: "VCH' Richmond medical health officer supports OPS",
+    media: {
+      name: "VCH' Richmond medical health officer supports OPS",
+      source: {
+        url: "https://raw.githubusercontent.com/voices-bridges/voices-against-drugs/main/static/img/VCH-support-Richmond-OPS.jpg",
+      },
+      type: "IMAGE",
+    },
+    url: "https://globalnews.ca/news/10295530/richmond-drug-site-vch/",
+    cardDetailedText: `In a letter to the mayor and council, VCH's Richmond medical health officer expressed support for overdose prevention services — both injection and inhalation — in the city, along with an “analysis in order to identify a range of solutions that may be appropriate to our local context.”`,
+  },
+  {
+    title: "2024-02-05",
     cardTitle: "The Motion 1st reading passed",
     cardSubtitle: "8 in favor, 1 against",
     media: {
